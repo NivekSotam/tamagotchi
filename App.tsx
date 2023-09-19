a7a91b06c4c0082f1f5eba7481bb9631f626601d
NivekSotam <kevin.matoss.km@gmail.com>
2023-09-18 21:42:04 -0300

    fix: pages

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Login from './screens/Login';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Register from './screens/Register';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Login />
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
